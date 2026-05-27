6333427111860d603e549c578fee452649d083fe
AdityaGaur26 <adityagaur2609@gmail.com>
2026-05-28 04:00:19 +0530

    restart feature

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const board = document.querySelector(".board")
 const modal = document.querySelector(".modal")
 const startBtn = document.querySelector(".start-btn")
+const resetBtn = document.querySelector(".reset-btn")
+const startGame = document.querySelector(".startgame")
+const gameover = document.querySelector(".gameover")
+
 
 startBtn.addEventListener("click", () => {
     modal.style.display = "none"
@@ -52,8 +56,9 @@ function render() {
     }
 
     if (snake[0].x === 0 || snake[0].x >= rows || snake[0].y === 0 || snake[0].y >= cols) {
-        alert("Game Over")
+
         clearInterval(moving)
+        resetGame()
     }
 
     snake.forEach(segment => {
@@ -76,15 +81,32 @@ function render() {
     if (snake[0].x === food.x && snake[0].y === food.y) {
         blocks[`${food.x}-${food.y}`].classList.remove("food")
         food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
-            blocks[`${food.x}-${food.y}`].classList.add("food")
-                snake.unshift(head)
-
-        }
-
-        
+        blocks[`${food.x}-${food.y}`].classList.add("food")
+        snake.unshift(head)
 
     }
 
+
+
+}
+
+function resetGame() {
+    modal.style.display = "flex"
+    startGame.style.display = "none"
+    gameover.style.display = "flex"
+
+    resetBtn.addEventListener("click", () => {
+        modal.style.display = "none"
+        snake = [{ x: 1, y: 3 }]
+        direction = "right"
+        blocks[`${food.x}-${food.y}`].classList.remove("food")
+        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+        moving = setInterval(() => {
+            render()
+        }, 400)
+        
+    })
+}
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight" || e.key === "d") {
