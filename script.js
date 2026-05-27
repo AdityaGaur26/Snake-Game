b99b710124ac0d43ce4ea697b62aeed778a3704a
AdityaGaur26 <adityagaur2609@gmail.com>
2026-05-28 05:01:10 +0530

    finish game

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const resetBtn = document.querySelector(".reset-btn")
 const startGame = document.querySelector(".startgame")
 const gameover = document.querySelector(".gameover")
 
+const high = document.querySelector("#highscore")
+const currentScore = document.querySelector("#score")
+const currentTime = document.querySelector("#time")
+
+let score = 0
+let time = `00-00`
+let highScore = localStorage.getItem("highscore") || 0
+high.textContent = `${highScore}`
+currentScore.textContent = `${score}`
+currentTime.textContent = `${time}`
+
+let timer = null
+
+timer = setInterval(() => {
+    let [minutes, seconds] = time.split("-").map(Number)
+    seconds++
+    if (seconds === 60) {
+        minutes++
+        seconds = 0
+    }
+    time = `${minutes}-${seconds}`
+    currentTime.textContent = `${time}`
+}, 1000)
 
 startBtn.addEventListener("click", () => {
     modal.style.display = "none"
@@ -35,7 +58,6 @@ for (let row = 0; row < rows; row++) {
         let block = document.createElement("div")
         block.classList.add("block")
         board.appendChild(block)
-        block.textContent = `${row}-${col}`
         blocks[`${row}-${col}`] = block
     }
 }
@@ -55,58 +77,97 @@ function render() {
         head = { x: snake[0].x - 1, y: snake[0].y }
     }
 
-    if (snake[0].x === 0 || snake[0].x >= rows || snake[0].y === 0 || snake[0].y >= cols) {
+    if (
+        head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols
+    ){
+    clearInterval(moving)
+    resetGame()
+}
 
-        clearInterval(moving)
-        resetGame()
-    }
-
-    snake.forEach(segment => {
-        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
-    })
-    snake.unshift(head)
-    snake.pop()
-
-
-    snake.forEach(segment => {
-        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
-    })
+snake.forEach(segment => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+})
+snake.unshift(head)
+snake.pop()
 
 
-    /* Food */
+snake.forEach(segment => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+})
 
 
+/* Food */
+
+
+blocks[`${food.x}-${food.y}`].classList.add("food")
+
+if (snake[0].x === food.x && snake[0].y === food.y) {
+    blocks[`${food.x}-${food.y}`].classList.remove("food")
+    food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
     blocks[`${food.x}-${food.y}`].classList.add("food")
+    snake.push(head)
 
-    if (snake[0].x === food.x && snake[0].y === food.y) {
-        blocks[`${food.x}-${food.y}`].classList.remove("food")
-        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
-        blocks[`${food.x}-${food.y}`].classList.add("food")
-        snake.unshift(head)
+    score += 10
+    currentScore.textContent = `${score}`
+
+    if (score > highScore) {
+        highScore = score
+        localStorage.setItem("highscore", highScore)
 
     }
+
+}
 
 
 
 }
-
 function resetGame() {
     modal.style.display = "flex"
-    startGame.style.display = "none"
     gameover.style.display = "flex"
-
-    resetBtn.addEventListener("click", () => {
-        modal.style.display = "none"
-        snake = [{ x: 1, y: 3 }]
-        direction = "right"
-        blocks[`${food.x}-${food.y}`].classList.remove("food")
-        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
-        moving = setInterval(() => {
-            render()
-        }, 400)
-        
-    })
+    startGame.style.display = "none"
+    if (score > highScore) {
+        highScore = score
+        localStorage.setItem("highscore", highScore)
+        high.textContent = `${highScore}`
+    }
 }
+resetBtn.addEventListener("click", () => {
+
+    clearInterval(moving);
+
+    modal.style.display = "none"
+
+    snake = [{ x: 1, y: 3 }]
+
+    direction = "down"
+
+    blocks[`${food.x}-${food.y}`].classList.remove("food")
+
+    food = {
+
+        x: Math.floor(Math.random() * rows),
+
+        y: Math.floor(Math.random() * cols)
+
+    }
+
+    moving = setInterval(() => {
+
+        render()
+
+    }, 400)
+
+    score = 0
+
+    time = `00-00`
+
+    currentScore.textContent = `${score}`
+
+    currentTime.textContent = `${time}`
+
+    high.textContent = `${highScore}`
+
+})
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight" || e.key === "d") {
